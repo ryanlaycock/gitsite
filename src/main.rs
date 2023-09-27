45a91030be0a35a934db1080cfbede324209ecfe
ryanlaycock <ryan.laycock@hotmail.co.uk>
2023-09-28 00:17:23 +0100

    Added some example webpages, including a style sheet

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,4 @@
-use std::fs::File;
-use std::{path::PathBuf};
-use std::fmt;
+use std::{path::PathBuf, fmt, env};
 use std::collections::BTreeMap;
 
 use actix_files::NamedFile;
@@ -8,6 +6,8 @@ use actix_web::{get, web, App, Error, HttpRequest, HttpServer, ResponseError};
 
 use serde::Deserialize;
 use serde_yaml::{self};
+
+use std::sync::Arc;
 
 #[derive(Deserialize, Debug, Clone)]
 struct Page {
@@ -24,6 +24,7 @@ struct Config {
 #[derive(Debug, Clone)]
 struct AppData {
     site_config: Config,
+    local_files_dir: String,
 }
 
 #[derive(Debug)]
@@ -37,36 +38,20 @@ impl fmt::Display for CustomError {
 
 impl ResponseError for CustomError {}
 
-
-#[get("/{path:.*}")]
-async fn fetch_file(data: web::Data<AppData>, req: HttpRequest) -> Result<NamedFile, CustomError> {
-    println!("Fecthing file {} from config {:?}", req.path(), data.site_config);
-    
-    if let Some(page) = data.site_config.content.get(req.path()) {
-        let web_path = "/home/ryan/gh-rl/github-website/".to_string() + &page.filePath;
-        println!("Opening {:?}", web_path);
-        let path: PathBuf = web_path.into();
-
-        if let Ok(file) = NamedFile::open(&path) {
-            return Ok(file);
-        } else {
-            // Value is not a valid file path
-            return Err(CustomError("Invalid file path".to_string()));
-        }
-    }
-
-    Err(CustomError("Key not found".to_string()))
-}
-
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    std::env::set_var("RUST_LOG", "debug");
+    let local_files_dir = env::var("LOCAL_FILES_DIR").expect("LOCAL_FILES_DIR not found");
+    let cfg_file = env::var("CONFIG_FILE").expect("CFG_FILE not found");
+
     env_logger::init();
 
-    let config_str: String = get_config_str().unwrap();
+    let config_str: String = get_config_str(cfg_file).unwrap();
     let result: Result<Config, serde_yaml::Error> = serde_yaml::from_str(&config_str);
     let site_config = result.unwrap();
-    let app_data = AppData { site_config: site_config };
+    let app_data = AppData { 
+        site_config: site_config,
+        local_files_dir: local_files_dir,
+    };
 
     println!("{:?}", app_data);
     println!("{:?}", web::Data::new(app_data.clone()));
@@ -81,9 +66,30 @@ async fn main() -> std::io::Result<()> {
     .await
 }
 
-fn get_config_str() -> Result<String, Error> {
-    let web_path = "/home/ryan/gh-rl/github-website/config.yaml";
-    let f = std::fs::read_to_string(web_path);
+#[get("/{path:.*}")]
+async fn fetch_file(data: web::Data<AppData>, req: HttpRequest) -> Result<NamedFile, CustomError> {
+    println!("Fetching file {} from config {:?}", req.path(), data.site_config);
+    
+    if let Some(page) = data.site_config.content.get(req.path()) {
+        let mut web_path = data.local_files_dir.clone();
+        web_path.push_str(&page.filePath);
+        println!("Opening {:?}", web_path);
+        
+        let path: PathBuf = web_path.into();
+
+        if let Ok(file) = NamedFile::open(&path) {
+            return Ok(file);
+        } else {
+            // Value is not a valid file path
+            return Err(CustomError("Invalid file path".to_string()));
+        }
+    }
+
+    Err(CustomError("Key not found".to_string()))
+}
+
+fn get_config_str(file_location: String) -> Result<String, Error> {
+    let f = std::fs::read_to_string(file_location);
     let file_str = match f {
         Ok(v) => v,
         Err(e) => {
