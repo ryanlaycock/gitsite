8b0334b3768163c445dba4077e12da24c5a652ad
ryanlaycock <ryan.laycock@hotmail.co.uk>
2023-10-08 02:57:57 +0100

    Sending static files as static files

--- src/handlers.rs
+++ src/handlers.rs
@@ -4,35 +4,28 @@ use crate::page_domain::{
     update_and_get_lib_page,
 };
 
+use axum::routing::head;
 use axum::{
     response::IntoResponse,
     Json,
     extract::{Path, State},
-    http::StatusCode,
+    http::{StatusCode, header}
 };
-
-
 use std::sync::Arc;
 
-pub async fn get_lib_handler(Path(path): Path<String>, State(shared_state): State<Arc<AppData>>) ->  Result<impl IntoResponse, (StatusCode, Json<serde_json::Value>)> {
+pub async fn get_lib_handler(Path(path): Path<String>, State(shared_state): State<Arc<AppData>>) ->  Result<impl IntoResponse, (StatusCode, [(&'static str, &'static str); 1], &'static str)> {
     println!("Request for lib file at {:?}", path);
     let app_data = shared_state;
        
     match update_and_get_lib_page(app_data.to_owned(), &path).await {
         Ok(memory_page) => {
-            let json_response = serde_json::json!({
-                "data": memory_page.content,
-                "lastUpdatedAt": memory_page.last_updated_at,
-            });
-            return Ok((StatusCode::OK, Json(json_response)));
+            
+            return Ok((StatusCode::OK, [("Content-Type", "text")], memory_page.content));
         },
-        Err(err) => {
-            let error_response = serde_json::json!({
-                "message": err.to_string(),
-            });
-            return Err((StatusCode::NOT_FOUND, Json(error_response)));
+        Err(_) => {
+            return Err((StatusCode::NOT_FOUND, [("", "")], ""));
         }
-    }
+    }    
 }
 
 pub async fn get_content_handler(Path(path): Path<String>, State(shared_state): State<Arc<AppData>>) ->  Result<impl IntoResponse, (StatusCode, Json<serde_json::Value>)> {
